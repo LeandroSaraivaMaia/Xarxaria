bb1685a6c959cb8b2793d70134b1d32f7c53f7d4
Leandro <Leandro.SARAIVA-MAIA@cpnv.ch>
2019-01-10 09:19:05 +0100

    Son lors du passage de la souris
    + Mise en place du code frmCombat.cs (architecture et commentaires)

--- Code source/Xarxaria/Forms/frmCombat.cs
+++ Code source/Xarxaria/Forms/frmCombat.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/**
+* \file      frmCombat.cs
+* \author    Johan Voland & Leandro Saraiva Maia
+* \version   1.0
+* \date      December 13. 2018
+* \brief     Combat form of the game
+*
+* \details   This form is opened when a combat is asked. It takes the enemy id as argument and it purpose is to enable the player
+* to fight an enemy.
+*/
+
+#region using
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +19,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+#endregion
 
 namespace Xarxaria
 {
     public partial class frmCombat : Form
     {
-        public frmCombat()
+        #region private attributes
+        int idEnemy;
+        #endregion
+
+        #region constructor
+        /// <summary>
+        /// The combat form constructor
+        /// </summary>
+        /// <param name="idEnemy"></param>
+        public frmCombat(int idEnemy)
         {
             InitializeComponent();
+
+            //Get enemy id
+            this.idEnemy = idEnemy;
+
+            //Wire mouse enter events for sound effect
+            cmdNextTurn.MouseEnter += cmd_MouseEnter;
         }
+        #endregion
+
+        #region events
+        /// <summary>
+        /// Click on next turn button :
+        /// 
+        /// Play the next turn
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cmdNextTurn_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region sound events
+        /// <summary>
+        /// The mouse enter in a button :
+        /// 
+        /// Play hover sound
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cmd_MouseEnter(object sender, EventArgs e)
+        {
+            Program.playHoverSound();
+        }
+        #endregion
     }
 }
